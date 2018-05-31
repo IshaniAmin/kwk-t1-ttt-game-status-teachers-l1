33644ee77e40b1e99f3eac230145fdbdcd1408d6
Ishani <ishaniamin@gmail.com>
2018-05-31 18:43:41 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -10,6 +10,7 @@ WIN_COMBINATIONS = [
   [0,4,8], [2,4,6]
   ]
 
+
 def won?(board)
   WIN_COMBINATIONS.find do |comb|
     if position_taken?(board,comb[0])
@@ -19,18 +20,22 @@ def won?(board)
   end
 end
 
+
 def full?(board)
   ind = [0,1,2,3,4,5,6,7,8]
   ind.all? {|i| position_taken?(board, i)}
 end
 
+
 def draw?(board)
   !won?(board) && full?(board)
 end
 
+
 def over?(board)
   won?(board) || full?(board) || draw?(board)
 end
+
 
 def winner(board)
   if won?(board)
